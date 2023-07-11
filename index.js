b8da400d4fd395892be12d49b749482ae906ae6a
Dung ONEADX <ngdung7501@gmail.com>
2023-07-11 18:10:42 +0700

    Like, comment

--- index.js
+++ index.js
@@ -88,53 +88,118 @@ const waitForSelector = async (page, selector, options = { timeout: 3000 }) => {
     //   'div[data-mcomponent="MContainer"] > div[role="button"] div[data-mcomponent="ServerTextArea"][data-type="text"] .fl.ac.am .native-text';
     // const clickIndexes = [4, 2, 5];
 
-    //Xem video, xem bạn bè, xem thông báo
-    await delay(10000);
-    const elementFr =
-      'div[data-mcomponent="MContainer"] > div[role="button"] div[data-mcomponent="ServerTextArea"][data-type="text"] .fl.ac.am .native-text';
-    const clickIndexes = [4];
+    // //Xem video, xem bạn bè, xem thông báo
+    // await delay(10000);
+    // const elementFr =
+    //   'div[data-mcomponent="MContainer"] > div[role="button"] div[data-mcomponent="ServerTextArea"][data-type="text"] .fl.ac.am .native-text';
+    // const clickIndexes = [4];
 
-    await page.waitForSelector(elementFr);
-    const elements = await page.$$(elementFr);
-    for await (const index of clickIndexes) {
-      // for (const index of clickIndexes) {
-      if (index < elements.length) {
-        console.log("index", index);
-        await delay(5000);
-        try {
-          elements[index].click();
-          await delay(3000);
-          const lastPosition1 = await scrollPageToBottom(page, {
-            size: 500,
-            delay: 1500,
-          });
+    // await page.waitForSelector(elementFr);
+    // const elements = await page.$$(elementFr);
+    // for await (const index of clickIndexes) {
+    //   // for (const index of clickIndexes) {
+    //   if (index < elements.length) {
+    //     console.log("index", index);
+    //     await delay(5000);
+    //     try {
+    //       elements[index].click();
+    //       await delay(3000);
+    //       const lastPosition1 = await scrollPageToBottom(page, {
+    //         size: 500,
+    //         delay: 1500,
+    //       });
 
-          await delay(3000);
-          const lastPosition3 = await scrollPageToTop(page, {
-            size: 500,
-            delay: 1500,
-          });
-        } catch (error) {
-          console.log("error", index, error);
-        }
-        await delay(5000);
-        try {
-          await page.goBack();
-        } catch (error) {
-          console.log("error goBack", index, error);
-        }
-        console.log("done", index);
-      }
-    }
-    await page.waitForNavigation();
-    const lastPosition2 = await scrollPageToBottom(page, {
-      size: 500,
-      delay: 1500,
-    });
+    //       await delay(3000);
+    //       const lastPosition3 = await scrollPageToTop(page, {
+    //         size: 500,
+    //         delay: 1500,
+    //       });
+    //     } catch (error) {
+    //       console.log("error", index, error);
+    //     }
+    //     await delay(5000);
+    //     try {
+    //       await page.goBack();
+    //     } catch (error) {
+    //       console.log("error goBack", index, error);
+    //     }
+    //     console.log("done", index);
+    //   }
+    // }
+    // await page.waitForNavigation();
+    // const lastPosition2 = await scrollPageToBottom(page, {
+    //   size: 500,
+    //   delay: 1500,
+    // });
 
     //browser.close();
 
-    //Like, bình luận bài viết
+    // //Like bài viết
+    // await delay(10000);
+    // const elementLike =
+    //   'div[data-mcomponent="MContainer"] > div[data-type="container"] > div[data-focusable="true"] > div[data-mcomponent="TextArea"] .native-text';
+    // const clickLikes = [1];
+    // await page.waitForSelector(elementLike);
+    // const elementLikes = await page.$$(elementLike);
+
+    // // elementsLikes[1].click();
+
+    // for await (const index of clickLikes) {
+    //   //for (const index of clickLikes) {
+    //   if (index < elementLikes.length) {
+    //     console.log("index", index);
+    //     await delay(5000);
+    //     try {
+    //       elementLikes[index].click();
+    //       await delay(3000);
+    //       const lastPosition1 = await scrollPageToBottom(page, {
+    //         size: 500,
+    //         delay: 1500,
+    //       });
+
+    //       await delay(3000);
+    //       const lastPosition3 = await scrollPageToTop(page, {
+    //         size: 300,
+    //         delay: 1500,
+    //       });
+    //     } catch (error) {
+    //       console.log("error", index, error);
+    //     }
+    //     // await delay(5000);
+    //     // try {
+    //     //   await page.goBack();
+    //     // } catch (error) {
+    //     //   console.log("error goBack", index, error);
+    //     // }
+    //     console.log("done", index);
+    //   }
+    // }
+
+    //Bình luận bài viết
+    await delay(10000);
+    const elementComment =
+      'div[data-mcomponent="MContainer"] > div[data-type="container"] > div[data-focusable="true"] > div[data-mcomponent="TextArea"] .native-text';
+    //const clickComments = [2];
+    await page.waitForSelector(elementComment);
+    const elementComments = await page.$$(elementComment);
+    elementComments[2].click();
+    await delay(5000);
+    const eleComment =
+      'div[id="screen-root"] > div[data-adjust-on-keyboard-shown="true"] > div[data-shift-on-keyboard-shown="true"] > div[data-mcomponent="MContainer"] textarea[type="text"]';
+    //const clickComments = [2];
+    await page.waitForSelector(eleComment);
+    const eleComments = await page.$(eleComment);
+    eleComments.click();
+    await page.type(eleComment, ".");
+
+    await delay(5000);
+    const eleSend =
+      'div[id="screen-root"] > div[data-adjust-on-keyboard-shown="true"] > div[data-shift-on-keyboard-shown="true"] > div[data-mcomponent="MContainer"] div:not(.hidden) > div > div[role="button"]';
+    //const clickComments = [2];
+    await page.waitForSelector(eleSend);
+    const eleSends = await page.$(eleSend);
+    eleSends.click();
+    //await page.type(eleComment, ".");
 
     //Xem tin
   } catch (error) {
